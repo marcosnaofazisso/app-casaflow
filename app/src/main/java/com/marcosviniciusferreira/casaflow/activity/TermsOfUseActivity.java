3c567faccff04eab5ceae6ef7b4ac81c8e99539d
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2024-02-06 09:25:45 -0300

    fix design for dark mode

--- app/src/main/java/com/marcosviniciusferreira/casaflow/activity/TermsOfUseActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/casaflow/activity/TermsOfUseActivity.java
@@ -5,8 +5,11 @@ import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.marcosviniciusferreira.casaflow.R;
 
@@ -14,14 +17,27 @@ public class TermsOfUseActivity extends AppCompatActivity {
 
     private Button buttonReadTerms;
 
+    private TextView textTerms;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_of_use);
         setRequestedOrientation(SCREEN_ORIENTATION_PORTRAIT);
 
+        textTerms = findViewById(R.id.textAllTermsOfUse);
+
         getSupportActionBar().setTitle("CasaFlow - Termos de Uso");
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        // Check for Dark Mode
+        int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        boolean isDarkMode = nightModeFlags == Configuration.UI_MODE_NIGHT_YES;
+
+        if (isDarkMode) {
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.purple_200)));
+            textTerms.setTextColor(getResources().getColor(R.color.purple_500));
+        }
 
         buttonReadTerms = findViewById(R.id.buttonReadTerms);
 
